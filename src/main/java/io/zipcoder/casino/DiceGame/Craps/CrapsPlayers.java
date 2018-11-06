694a5250e0338e598821f49438ee281a0afeb22d
Christian Sheridan <christiansheridan@zipcoders-MacBook-Pro-13.local>
2018-11-06 17:36:26 -0500

    first additions to craps

---
+++ src/main/java/io/zipcoder/casino/DiceGame/Craps/CrapsPlayers.java
@@ -0,0 +1,40 @@
+package io.zipcoder.casino.DiceGame.Craps;
+
+import io.zipcoder.casino.Player;
+
+public class CrapsPlayers {
+
+        private Player player;
+
+        private int initialBet;
+        private int rollValue;
+        private int betPot;
+
+        public CrapsPlayers(Player player) {
+            this.player = player;
+            this.initialBet = 0;
+            this.rollValue = 0;
+            this.betPot = 0;
+        }
+
+        public Player getPlayer() {
+            return player;
+        }
+
+        public int getInitialBet() {
+            return initialBet;
+        }
+
+        public void addToBetPot(int amount) {
+            betPot += amount;
+        }
+
+        public int getBetPot() {
+            return betPot;
+        }
+
+        public void setInitialBet(int amount){
+            this.initialBet = amount;
+        }
+
+    }
